c22943a4304bbd841b1f8fda20e5ee98f63fc1f6
Hasnat Shahriyar <hasnatshahriyarnahin1@gmail.com>
2026-01-22 16:30:26 +0600

    navitems component added

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import Navbar from "../pages/shared/Navbar/Navbar";
 
 const RootLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
